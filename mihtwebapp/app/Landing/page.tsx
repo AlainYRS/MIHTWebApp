743ed0085559a3dbb8c55402d2672aeecaf5f321
Alain Rivera <galain0106@gmail.com>
2024-07-26 14:42:06 -0400

    General Adjustments

--- mihtwebapp/app/Landing/page.tsx
+++ mihtwebapp/app/Landing/page.tsx
@@ -18,11 +18,11 @@ import Link from "next/link";
             </div>
             </div>
             <div className={styles.Links}>
-                <Link href='https://www.hottacosrestaurant.com/Reviews' className={styles.Link}>Califica el Servicio</Link>
-                <Link href='https://hottacosrestaurant.com/' className={styles.Link}>Consulta el Menú</Link>
-                <Link href='https://www.hottacosrestaurant.com/Raffles' className={styles.Link}>Participa en Rifas</Link>
-                <Link href='https://www.eventbrite.com/e/ncg-wrestling-presents-lucha-libra-supreme-tickets-950830508647' target="_blank" className={styles.Link}>Festeja el 15 de Septiembre con Lucha Libre en <br/><strong>El Patron Bar & Grill</strong></Link>
-                <h1 className={styles.Link}>Free WiFi Netrwork: Hot Tacos Guest / VIP Guest</h1>
+                <Link href='https://www.hottacosrestaurant.com/Reviews' className={styles.Link}>Califica el Servicio / Rate your Experience</Link>
+                <Link href='https://hottacosrestaurant.com/' className={styles.Link}>Menu</Link>
+                <Link href='https://www.hottacosrestaurant.com/Raffles' className={styles.Link}>Participa en Rifas / Be a part of our raffles</Link>
+                <Link href='https://www.eventbrite.com/e/ncg-wrestling-presents-lucha-libra-supreme-tickets-950830508647' target="_blank" className={styles.Link}>Festeja con Lucha Libre / Celebrate with Lucha Libre<br/><strong>El Patron Bar & Grill (15 de Septiembre)</strong></Link>
+                <h1 className={styles.Link}>Search the free WiFi Netrwork: Hot Tacos Guest / VIP Guest</h1>
             </div>
         </div>
     )
