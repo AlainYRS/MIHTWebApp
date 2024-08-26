9573f2b7fb680b139fc94d41025c33c4b5d36cf4
Alain Rivera <galain0106@gmail.com>
2024-08-26 14:23:03 -0400

    General Adjustments

--- mihtwebapp/app/Landing/page.tsx
+++ mihtwebapp/app/Landing/page.tsx
@@ -18,8 +18,12 @@ import Link from "next/link";
             </div>
             </div>
             <div className={styles.Links}>
+                <div className={styles.SMR}>
+                    <a href={'https://www.facebook.com/HotTacos?mibextid=LQQJ4d'} target="_blank" rel="noopener noreferrer"><img style={{borderRadius:'7px', margin:'0px 14px'}} width={30} height={30} alt={'Social Media'} src="/Icons/FacebookIcon.png" /></a>
+                    <a href={'https://www.instagram.com/hottacosrestaurant?igsh=Y3hkMHBhNDk5M2Rl'} target="_blank" rel="noopener noreferrer"><img style={{borderRadius:'7px', margin:'0px 14px'}} width={30} height={30} alt={'Social Media'} src="/Icons/InstaIcon.png" /></a>
+                </div>
                 <Link href='https://www.hottacosrestaurant.com/Reviews' className={styles.Link}>Califica el Servicio / Rate your Experience</Link>
-                <Link href='https://hottacosrestaurant.com/' className={styles.Link}>Menu</Link>
+                <Link href='https://hottacosrestaurant.com/' className={styles.Link}>Menus</Link>
                 <Link href='https://www.hottacosrestaurant.com/Raffles' className={styles.Link}>Participa en Rifas / Be a part of our raffles</Link>
                 <Link href='https://www.eventbrite.com/e/ncg-wrestling-presents-lucha-libra-supreme-tickets-950830508647' target="_blank" className={styles.Link}>Festeja con Lucha Libre / Celebrate with Lucha Libre<br/><strong>El Patron Bar & Grill (15 de Septiembre)</strong></Link>
                 <h1 className={styles.Link}>Search the free WiFi Netrwork: Hot Tacos Guest / VIP Guest</h1>
