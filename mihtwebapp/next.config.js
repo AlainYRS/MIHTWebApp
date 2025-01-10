a3e836e5494a3686837a5b357a35d31c5cf397ca
Alain Rivera <galain0106@gmail.com>
2025-01-10 09:39:27 -0500

    General Adjustments

--- mihtwebapp/next.config.js
+++ mihtwebapp/next.config.js
@@ -8,4 +8,17 @@ const withPWA = require("@ducanh2912/next-pwa").default(
     }
 );
   
-module.exports = withPWA();
+// module.exports = withPWA();
+
+module.exports = withPWA({
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "firebasestorage.googleapis.com",
+          port: "",
+          pathname: "/v0/b/mihottacoswebapp.appspot.com/o/**",
+        },
+      ],
+    },
+  });
