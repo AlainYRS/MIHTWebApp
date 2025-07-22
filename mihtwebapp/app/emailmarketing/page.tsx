4a4d556d02f3f9c24eb9d4ec16f05891c4d5bb0e
AYRS <galain0106@gmail.com>
2025-07-22 17:20:19 -0400

    General Adjustments

---
+++ mihtwebapp/app/emailmarketing/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React, { useState, useEffect } from "react";
+
+export default function emailmarketing(){
+        
+    return(
+        <a>
+            Hola Mundo
+       </a>
+    )
+}
